976d799c42ddd342972c5614c9d36edfa22719a8
John Reese <jreese@plex.com>
2015-09-23 14:32:27 -0400

    Initial commit

---
+++ CommandSimon/CommandSimon/CommandFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandSimon.Commands;
+
+namespace CommandSimon
+{
+  public class CommandFactory
+  {
+    public ICommand CreateCommand(string command)
+    {
+      return null;
+    }
+  }
+}
