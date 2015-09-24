2fee73f896488f4b2245a3740cfd3a8162d114a1
John Reese <jpreese@mtu.edu>
2015-09-24 00:04:32 -0400

    Add some driver code to the main method

--- CommandSimon/Program.cs
+++ CommandSimon/Program.cs
@@ -1,15 +1,24 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace CommandSimon
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      var commandFactory = new CommandFactory();
-      var command = commandFactory.CreateCommand(args[0]);
+        static void Main(string[] args)
+        {
+            var commandText = "jump";
 
-      Console.ReadKey();
+            var commandFactory = new CommandFactory();
+            var command = commandFactory.CreateCommand(commandText);
+            var com = commandFactory.CreateCommand(commandText);
+            command.Execute();
+            com.Execute();
+
+            var woo = new CommandFactory();
+            woo.CreateCommand(commandText);
+
+            Console.ReadKey();
+        }
     }
-  }
 }
