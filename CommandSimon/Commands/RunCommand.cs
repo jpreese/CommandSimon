8202de0df6ce2909fcde5e3a72dba4f259900211
John Reese <jpreese@mtu.edu>
2015-09-24 00:03:37 -0400

    Initial commit

---
+++ CommandSimon/Commands/RunCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandSimon.Commands
+{
+    public class RunCommand : ICommand
+    {
+        public void Execute()
+        {
+            System.Console.WriteLine("run...");
+        }
+    }
+}
