d5fa823a804d9a13f9e44bd5896552e14daaa927
John Reese <jpreese@mtu.edu>
2015-09-24 00:04:15 -0400

    Load all classes that implement ICommand

--- CommandSimon/CommandFactory.cs
+++ CommandSimon/CommandFactory.cs
@@ -1,17 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 using CommandSimon.Commands;
 
 namespace CommandSimon
 {
-  public class CommandFactory
-  {
-    public ICommand CreateCommand(string command)
+    public class CommandFactory
     {
-      return null;
+        private static readonly Lazy<IList<ICommand>> _commandDictionary = new Lazy<IList<ICommand>>(LoadCommands);
+
+        public ICommand CreateCommand(string command)
+        {
+            var commands = _commandDictionary.Value;
+            return commands[0];
+        }
+ 
+        private static IList<ICommand> LoadCommands()
+        {
+            return Assembly.GetExecutingAssembly()
+                .GetTypes()
+                .Where(t => t.GetInterfaces()
+                .Contains(typeof (ICommand)))
+                .Select(a => Activator.CreateInstance(a) as ICommand)
+                .ToList();
+        }
     }
-  }
 }
