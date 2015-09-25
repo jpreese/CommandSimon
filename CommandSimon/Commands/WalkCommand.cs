436fd25cba7c71959e6bbe098c3c21b4aff71f29
John Reese <jreese@plex.com>
2015-09-25 16:08:54 -0400

    Initial commit

---
+++ CommandSimon/Commands/WalkCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CommandSimon.Commands
+{
+  public class WalkCommand : ICommand
+  {
+    public void Execute()
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
